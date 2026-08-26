7ffe1d8152a5c34017b110e1b65f0d8ac161a254
qqw655 <245692480@qq.com>
2026-08-27 02:44:31 +0800

    高翻提前至W3启动：悬垂高翻空杆技术-轻重量渐进，高拉转技术辅助

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='yy-diary-v8';
+const CACHE='yy-diary-v9';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
