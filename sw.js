383fb1794f569d3f9278c4f03cc6765c507afbf9
qqw655 <245692480@qq.com>
2026-08-29 10:21:17 +0800

    强度升级：主项末组RIR按阶段（减脂1/增肌塑形0-1）+轻日超级组省时间

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='yy-diary-v13';
+const CACHE='yy-diary-v14';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
