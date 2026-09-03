1858b99386e434e749fa4cb70e2cc1bcbecabbac
qqw655 <245692480@qq.com>
2026-09-03 08:15:33 +0800

    蹲B前蹲改为轻容量深蹲（手腕无压迫，深蹲一周两次）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='yy-diary-v21';
+const CACHE='yy-diary-v22';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
